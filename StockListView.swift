e6390c2658644ac5d93ecab8b46d31a7db332bff
Developer <dev@example.com>
2026-04-23 21:27:41 +0800

    缩小按钮宽度适配屏幕

--- StockListView.swift
+++ StockListView.swift
@@ -340,7 +340,7 @@ struct SortMetricView: View {
                 .font(.system(size: 10))
                 .foregroundColor(isHighlighted ? color : .gray)
         }
-        .frame(width: 38)
+        .frame(width: 32)
         .padding(.vertical, 4)
         .background(isHighlighted ? color.opacity(0.2) : Color.clear)
         .cornerRadius(6)
